475dc2b40065b9a65c3af377f1826dfa771ea782
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2025-09-26 10:46:41 -0600

    fixes #1, fixes #2, Codigo completo para las TDA's a utilizar

--- Estructuras/ListaSimple.py
+++ Estructuras/ListaSimple.py
@@ -1,45 +1,20 @@
-class Nodo:
-    def __init__(self, dato=None):
-        self.dato = dato
-        self.siguiente = None
+from Estructuras.NodoCelda import NodoCelda
 
 class ListaSimple:
     def __init__(self):
         self.primero = None
-        self.ultimo = None
-        self.tamanio = 0
 
-    def estaVacia(self):
-        return self.primero is None
-
-    def agregarUltimo(self, dato):
-        nuevo = Nodo(dato=dato)
-        if self.estaVacia():
-            self.primero = self.ultimo = nuevo
+    def insertar(self, nodo_celda):
+        if not self.primero:
+            self.primero = nodo_celda
         else:
-            self.ultimo.siguiente = nuevo
-            self.ultimo = nuevo
-        self.tamanio += 1
+            actual = self.primero
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nodo_celda
 
-    def buscar(self, valor, atributo=None):
-        temp = self.primero
-        while temp is not None:
-            if atributo is None:
-                if temp.dato == valor:
-                    return temp
-            else:
-                if hasattr(temp.dato, atributo):
-                    if getattr(temp.dato, atributo) == valor:
-                        return temp
-            temp = temp.siguiente
-        return None
-
-    def recorrer(self):
-        temp = self.primero
-        while temp is not None:
-            print(str(temp.dato), end=" | ")
-            temp = temp.siguiente
-        print()
-
-    def graficarCelda(self, drones):
-        pass
+    def __iter__(self):
+        actual = self.primero
+        while actual:
+            yield actual
+            actual = actual.siguiente
--- Estructuras/NodoCelda.py
+++ Estructuras/NodoCelda.py
@@ -1,8 +1,7 @@
-class Celda():
-    def __init__(self, info):
-        #DATO
+class NodoCelda:
+    def __init__(self, info=None):
         self.info = info
-
-        #APUNTADORES
-        self.siguiente=None
-        self.anterior=None
+        self.siguiente = None    # Para lista doble: siguiente hilera o columna
+        self.anterior = None    # Para lista doble: anterior hilera o columna
+        # Para matriz de plantas (lista de listas), solo se usan siguiente/anterior para hileras,
+        # y para columnas solo siguiente.
